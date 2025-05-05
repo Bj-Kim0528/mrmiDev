ed4d5e7ad71afed53df6a8f4ef8076f1e7645412
EC2 Default User <bjking102409@gmail.com>
2025-05-05 05:51:09 +0000

    WebSocket005

--- app/models/message.rb
+++ app/models/message.rb
@@ -9,7 +9,7 @@ class Message < ApplicationRecord
     ChatChannel.broadcast_to(conversation, render_message)
   end
 
-  after_create_commit :broadcast_preview_update
+  after_create_commit :broadcast_preview_update, :broadcast_first_message
 
   private
   
@@ -25,6 +25,26 @@ class Message < ApplicationRecord
             viewer:       participant
           }
         )
+      )
+    end
+  end
+
+  def broadcast_first_message
+    # 이 메시지가 conversation의 첫 메시지라면
+    return unless conversation.messages.count == 1
+  
+    conversation.users.each do |participant|
+      html = ApplicationController.renderer.render(
+        partial: "conversations/conversation_preview",
+        locals: { conversation: conversation, viewer: participant }
+      )
+  
+      # 사용자별, conversations_{user.id} 스트림에 prepend
+      ActionCable.server.broadcast(
+        "conversations_#{participant.id}",
+        tag: :prepend,
+        target: "conversations_#{participant.id}", 
+        html: html
       )
     end
   end
