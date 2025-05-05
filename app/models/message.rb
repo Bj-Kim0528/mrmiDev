0a24261aca3885da5ae13ee35da7f2edb83e306a
EC2 Default User <bjking102409@gmail.com>
2025-05-05 10:36:55 +0000

    WebSocket006

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,50 +1,45 @@
+# app/models/message.rb
 class Message < ApplicationRecord
   belongs_to :conversation
   belongs_to :user
 
   validates :content, presence: true
 
-  after_create_commit do
-    # Action Cable 브로드캐스트
+  # 실시간 채팅창 브로드캐스트
+  after_create_commit :broadcast_message
+
+  # 인덱스 미리보기 갱신
+  after_create_commit :broadcast_preview_update
+
+  # 첫 메시지라면 대화 목록에 prepend
+  after_create_commit :broadcast_first_message, if: -> { conversation.messages.count == 1 }
+
+  private
+
+  def broadcast_message
     ChatChannel.broadcast_to(conversation, render_message)
   end
 
-  after_create_commit :broadcast_preview_update, :broadcast_first_message
-
-  private
-  
   def broadcast_preview_update
     conversation.users.each do |participant|
       ConversationsPreviewChannel.broadcast_to(
         participant,
-        id: ActionView::RecordIdentifier.dom_id(conversation),
+        id:   ActionView::RecordIdentifier.dom_id(conversation),
         html: ApplicationController.renderer.render(
           partial: "conversations/conversation_preview",
-          locals: {
-            conversation: conversation,
-            viewer:       participant
-          }
+          locals: { conversation: conversation, viewer: participant }
         )
       )
     end
   end
 
   def broadcast_first_message
-    # 이 메시지가 conversation의 첫 메시지라면
-    return unless conversation.messages.count == 1
-  
     conversation.users.each do |participant|
-      html = ApplicationController.renderer.render(
+      Turbo::StreamsChannel.broadcast_prepend_to(
+        participant,
+        target: "conversations_#{participant.id}",               # ul#conversations_#{user.id}
         partial: "conversations/conversation_preview",
         locals: { conversation: conversation, viewer: participant }
-      )
-  
-      # 사용자별, conversations_{user.id} 스트림에 prepend
-      ActionCable.server.broadcast(
-        "conversations_#{participant.id}",
-        tag: :prepend,
-        target: "conversations_#{participant.id}", 
-        html: html
       )
     end
   end
@@ -52,7 +47,7 @@ class Message < ApplicationRecord
   def render_message
     ApplicationController.renderer.render(
       partial: 'messages/message',
-      locals: { message: self }
+      locals:  { message: self }
     )
   end
 end
